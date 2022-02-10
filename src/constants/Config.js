20c40a5919f4bd4e95d900d0570023c2de3678e5
Luis Ramos <luisra.dev@gmail.com>
2022-02-09 22:45:31 -0500

    feat: se creó la carpeta de configuración que contiene los URL's del servicio

---
+++ src/constants/Config.js
@@ -0,0 +1,19 @@
+//Ip del servidor local
+const URL_BASE_LOCAL = 'http://192.168.123.3:3000';
+
+//Módulo de Configuración - Almacen
+export const GET_ALMACENES = URL_BASE_LOCAL + '/config/almacen';
+export const CREATE_ALMACEN = URL_BASE_LOCAL + '/config/almacen';
+export const GET_ID_ALMACEN = URL_BASE_LOCAL + '/config/almacen/';
+export const UPDATE_ALMACEN = URL_BASE_LOCAL + '/config/almacen/';
+export const DELETE_ALMACEN = URL_BASE_LOCAL + '/config/almacen/';
+
+//Módulo de Configuración - Productos
+
+export const GET_PRODUCTOS = URL_BASE_LOCAL + '/config/producto';
+export const CREATE_PRODUCTO = URL_BASE_LOCAL + '/config/producto';
+export const GET_ID_PRODUCTO = URL_BASE_LOCAL + '/config/producto/';
+export const UPDATE_PRODUCTO = URL_BASE_LOCAL + '/config/producto/';
+export const DELETE_PRODUCTO = URL_BASE_LOCAL + '/config/producto/';
+
+
