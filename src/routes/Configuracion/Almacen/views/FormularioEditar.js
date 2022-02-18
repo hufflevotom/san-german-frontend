0b6c73e378bf5b2ce24db29429260f52e1c689cd
Huffle Votom <ten.crypto@outlook.com>
2022-02-17 22:59:49 -0500

    feat: diseño formulario editar almacen

--- src/routes/Configuracion/Almacen/views/FormularioEditar.js
+++ src/routes/Configuracion/Almacen/views/FormularioEditar.js
@@ -5,90 +5,113 @@ import { useDispatch } from 'react-redux';
 import { setClear, setNombre, setUbicacion } from '../../../../appRedux/actions/Configuracion/Almacen';
 import { editarAlmacen, obtenerUnAlmacen } from '../controllers';
 import { useSelector } from 'react-redux';
+import { LeftOutlined } from '@ant-design/icons';
 
 export const FormularioEditarAlmacen = (props) => {
 
-    const { match: { params } } = props;
+  const { match: { params } } = props;
 
-    const almacen = useSelector(state => state.almacen);
-    const dispatch = useDispatch();
-    const formRef = useRef();
+  const almacen = useSelector(state => state.almacen);
+  const dispatch = useDispatch();
+  const formRef = useRef();
 
-    useEffect(() => {
-        obtenerUnAlmacen(params.id);
-    }, [params.id]);
+  useEffect(() => {
+    obtenerUnAlmacen(params.id);
+  }, [params.id]);
 
-    useEffect(() => {
-        formRef.current.setFieldsValue({ nombre: almacen.nombre, ubicacion: almacen.ubicacion });
-    }, [almacen])
+  useEffect(() => {
+    formRef.current.setFieldsValue({ nombre: almacen.nombre, ubicacion: almacen.ubicacion });
+  }, [almacen])
 
 
 
-    return (
-        <>
-            <Button type='primary' onClick={() => dispatch(setClear())}>
+  return (
+    <>
+      <Card
+        title={
+          <div
+            style={{
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "space-between",
+            }}
+          >
+            <div style={{ display: "flex", flexDirection: "row" }}>
+              <Button
+                type="link"
+                style={{ margin: 0, padding: 0 }}
+                onClick={() => dispatch(setClear())}
+              >
                 <Link to="/configuracion/almacen">
-                    Volver
+                  <LeftOutlined
+                    style={{ fontSize: "16px", margin: 0, paddingTop: "5px" }}
+                  />
                 </Link>
-            </Button>
+              </Button>
+              <h1 style={{ margin: "0 0 0 20px", paddingTop: "3px" }}>
+                Editar Almacen
+              </h1>
+            </div>
+            <div>
+              <Button
+                style={{ margin: 0 }}
+                loading={almacen.cargando}
+                disabled={
+                  almacen.nombre === '' || almacen.ubicacion === ''
+                    ? true
+                    : false
+                }
+                type="primary"
+                htmlType="submit"
+                onClick={() => editarAlmacen(params.id)}>
+                Actualizar
+              </Button>
+            </div>
+          </div>
+        }
+      >
+        <Form
+          ref={formRef}
+          name="wrap"
+          labelCol={{
+            flex: '110px',
+          }}
+          labelAlign="left"
+          labelWrap
+          wrapperCol={{
+            flex: 1,
+          }}
+          colon={false}
+          style={{ padding: "0 50px 0 50px" }}
+        >
+          <Form.Item
+            label="Nombre"
+            name="nombre"
+            rules={[
+              {
+                required: true,
+                message: 'El nombre es requerido',
 
-            <Card title="Editar Almacen">
-                <Form
-                    ref={formRef}
-                    name="wrap"
-                    labelCol={{
-                        flex: '110px',
-                    }}
-                    labelAlign="left"
-                    labelWrap
-                    wrapperCol={{
-                        flex: 1,
-                    }}
-                    colon={false}
-                >
-                    <Form.Item
-                        label="Nombre"
-                        name="nombre"
-                        rules={[
-                            {
-                                required: true,
-                                message: 'El nombre es requerido',
+              },
+            ]}
+          >
+            <Input onChange={(e) => dispatch(setNombre(e.target.value))} />
+          </Form.Item>
 
-                            },
-                        ]}
-                    >
-                        <Input onChange={(e) => dispatch(setNombre(e.target.value))} />
-                    </Form.Item>
-
-                    <Form.Item
-                        label="Ubicación"
-                        name="ubicacion"
-                        rules={[
-                            {
-                                required: true,
-                                message: 'La ubicación es requerido',
-                            },
-                        ]}
-                    >
-                        <Input onChange={(e) => dispatch(setUbicacion(e.target.value))} />
-                    </Form.Item>
-
-                    <Form.Item label=" ">
-                        <Button
-                            loading={almacen.cargando}
-                            disabled={
-                                almacen.nombre === '' || almacen.ubicacion === ''
-                                    ? true
-                                    : false
-                            }
-                            type="primary"
-                            htmlType="submit"
-                            onClick={() => editarAlmacen(params.id)}>
-                            Actualizar
-                        </Button>
-                    </Form.Item>
-                </Form>
-            </Card>
-        </>
-    )
+          <Form.Item
+            label="Ubicación"
+            name="ubicacion"
+            rules={[
+              {
+                required: true,
+                message: 'La ubicación es requerido',
+              },
+            ]}
+          >
+            <Input onChange={(e) => dispatch(setUbicacion(e.target.value))} />
+          </Form.Item>
+        </Form>
+      </Card>
+    </>
+  )
 }
