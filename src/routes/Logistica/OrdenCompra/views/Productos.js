2ce2a1c3cc497c370260a90dacb90cc1fdc9d8f9
Huffle Votom <ten.crypto@outlook.com>
2022-02-28 12:00:28 -0500

    feat: quitar collapse en orden de compra

--- src/routes/Logistica/OrdenCompra/views/Productos.js
+++ src/routes/Logistica/OrdenCompra/views/Productos.js
@@ -17,7 +17,7 @@ import { onChangeCOD, onSelectCOD, onChangeNOM, onSearchNOM } from "../controlle
 //Controllers
 // import { guardarFamilia, actualizarFamilia, obtenerFamilia } from "../controllers";
 
-export const Productos = ({opciones}) => {
+export const Productos = ({ opciones }) => {
 
   const formRef = createRef();
   const dispatch = useDispatch();
@@ -54,27 +54,19 @@ export const Productos = ({opciones}) => {
     <Form.List name="atributos" style={{ margin: 0, padding: 0 }}>
       {(fields, { add, remove }) => (
         <>
-          <Collapse
-            defaultActiveKey={['0']}
-            style={{ margin: 0, padding: 0, width: "100%" }}
-            // collapsible="disabled"
-            ghost
-          >
-            {fields.map(({ key, name, ...restField }) => (
-              <Collapse.Panel
-                key={key}
-                header={
-                  <Row
-                    style={{
-                      display: "flex",
-                      flexDirection: "row",
-                      margin: "0 20px 0 20px",
-                      padding: "0",
-                      alignItems: "start",
-                      width: "100%"
-                    }}
-                  >
-                    {/* <Col xs={3} style={{ padding: "0 5px 0 5px" }}>
+          {
+            fields.map(({ key, name, ...restField }) => (
+              <Row
+                style={{
+                  display: "flex",
+                  flexDirection: "row",
+                  margin: "0 20px 0 20px",
+                  padding: "0",
+                  alignItems: "start",
+                  width: "100%"
+                }}
+              >
+                {/* <Col xs={3} style={{ padding: "0 5px 0 5px" }}>
                             <Form.Item
                               {...restField}
                               name={[name, "codProducto"]}
@@ -92,152 +84,81 @@ export const Productos = ({opciones}) => {
                               />
                             </Form.Item>
                           </Col> */}
-                    <Col xs={13} style={{ padding: "0 5px 0 5px" }}>
-                      <Form.Item
-                        {...restField}
-                        name={[name, "desProducto"]}
-                        // rules={[{ required: true, message: 'Missing first name' }]}
-                        style={{ margin: 0, width: "100%" }}
-                      >
-                        {/* <Input placeholder="Descripción del producto" style={{ margin: 0 }} /> */}
-                        <AutoComplete
-                          // value={valueNOM}
-                          options={opciones}
-                          onSearch={onSearchNOM}
-                          onSelect={(e) => onSelectNOM(e, [name, "desProducto"])}
-                          onChange={onChangeNOM}
-                          style={{ width: '100%' }}
-                          placeholder="Descripción del producto"
-                        />
-                      </Form.Item>
-                    </Col>
-                    <Col xs={3} style={{ padding: "0 5px 0 5px" }}>
-                      <Form.Item
-                        {...restField}
-                        name={[name, "loteProducto"]}
-                        // rules={[{ required: true, message: 'Missing first name' }]}
-                        style={{ margin: 0, width: "auto" }}
-                      >
-                        <Input placeholder="Número de Lote" style={{ margin: 0 }} />
-                      </Form.Item>
-                    </Col>
-                    <Col xs={3} style={{ padding: "0 5px 0 5px" }}>
-                      <Form.Item
-                        {...restField}
-                        name={[name, "ubiProducto"]}
-                        // rules={[{ required: true, message: 'Missing first name' }]}
-                        style={{ margin: 0, width: "auto" }}
-                      >
-                        <Input placeholder="Ubicación" style={{ margin: 0 }} />
-                      </Form.Item>
-                    </Col>
-                    <Col xs={3} style={{ padding: "0 5px 0 5px" }}>
-                      <Form.Item
-                        {...restField}
-                        name={[name, "cantProducto"]}
-                        // rules={[{ required: true, message: 'Missing first name' }]}
-                        style={{ margin: 0, width: "auto" }}
-                      >
-                        <Input type="number" placeholder="Cantidad" style={{ margin: 0 }} />
-                      </Form.Item>
-                    </Col>
-                    <Col xs={2} style={{ padding: "0 5px 0 5px" }}>
-                      <Form.Item
-                        {...restField}
-                        name={[name, "unidadProducto"]}
-                        // rules={[{ required: true, message: 'Missing first name' }]}
-                        style={{ margin: 0, width: "auto" }}
-                      >
-                        {/* <Input placeholder="U/M" style={{ margin: 0 }} /> */}
-                        <Select
-                          style={{ width: '100% ', margin: '0' }}
-                          showSearch
-                          placeholder="U/M"
-                          optionFilterProp="children"
-                          // onChange={onChangeHabitación}
-                          onSearch={() => { }}
-                          filterOption={(input, option) =>
-                            option.children.toLowerCase().indexOf(input.toLowerCase()) >= 0
-                          }
-                        >
-                          <Select.Option value={"a"}>a</Select.Option>
-                          <Select.Option value={"a"}>a</Select.Option>
-                        </Select>
-                      </Form.Item>
-                    </Col>
-                  </Row>
-                }
-                extra={genExtra(remove, name)}
-                style={{ margin: 0, padding: 0, width: "100%" }}
-                expandIcon={({ isActive }) => <CaretRightOutlined rotate={isActive ? 90 : 0} />}
-              >
-                <Row style={{ margin: '0 0 0 20%' }}>
-                  <Col xs={10}>
-                    Atributo
-                  </Col>
-                  <Col xs={10}>
-                    Opcion
-                  </Col>
-                  <Col xs={4}>
-                    Imagen
-                  </Col>
-                </Row>
-                <div
-                  style={{ display: "flex", flexDirection: "column" }}
-                >
-                  {
-                    opcionSeleccionada.map(({ key, name, ...restField }) => (
-                      <Row style={{ margin: '10px 0 10px 20%', display: 'flex', alignItems: "center" }}>
-                        <Col xs={10}>
-                          {name}
-                        </Col>
-                        <Col xs={10}>
-                          <Form.Item
-                            {...restField}
-                            name={[name, "nombre"]}
-                            // rules={[{ required: true, message: 'Missing first name' }]}
-                            style={{ margin: 0, width: "100%" }}
-                          >
-                            <Select
-                              style={{ width: '100% ', margin: '0' }}
-                              showSearch
-                              placeholder="Opcion"
-                              optionFilterProp="children"
-                              // onChange={onChangeHabitación}
-                              onSearch={() => { }}
-                              filterOption={(input, option) =>
-                                option.children.toLowerCase().indexOf(input.toLowerCase()) >= 0
-                              }
-                            >
-                              <Select.Option value={"a"}>a</Select.Option>
-                              <Select.Option value={"a"}>a</Select.Option>
-                            </Select>
-                          </Form.Item>
-                        </Col>
-                        <Col xs={4} style={{ display: 'flex', gap: '10px' }}>
-                          Icon
-                          {/* <MinusCircleOutlined
-                                onClick={() => remove(name)}
-                              /> */}
-                        </Col>
-                      </Row>
-                    ))
-                  }
-                  {/* <Form.Item style={{ margin: '10px 0 10px 20%' }}>
-                          <Button
-                            type="dashed"
-                            onClick={() => add()}
-                            block
-                            icon={<PlusOutlined />}
-                            style={{ margin: 0 }}
-                          >
-                            Agregar Opciones
-                          </Button>
-                        </Form.Item> */}
-                </div>
-              </Collapse.Panel>
+                <Col xs={13} style={{ padding: "0 5px 0 5px" }}>
+                  <Form.Item
+                    {...restField}
+                    name={[name, "desProducto"]}
+                    // rules={[{ required: true, message: 'Missing first name' }]}
+                    style={{ margin: 0, width: "100%" }}
+                  >
+                    {/* <Input placeholder="Descripción del producto" style={{ margin: 0 }} /> */}
+                    <AutoComplete
+                      // value={valueNOM}
+                      options={opciones}
+                      onSearch={onSearchNOM}
+                      onSelect={(e) => onSelectNOM(e, [name, "desProducto"])}
+                      onChange={onChangeNOM}
+                      style={{ width: '100%' }}
+                      placeholder="Descripción del producto"
+                    />
+                  </Form.Item>
+                </Col>
+                <Col xs={3} style={{ padding: "0 5px 0 5px" }}>
+                  <Form.Item
+                    {...restField}
+                    name={[name, "loteProducto"]}
+                    // rules={[{ required: true, message: 'Missing first name' }]}
+                    style={{ margin: 0, width: "auto" }}
+                  >
+                    <Input placeholder="Número de Lote" style={{ margin: 0 }} />
+                  </Form.Item>
+                </Col>
+                <Col xs={3} style={{ padding: "0 5px 0 5px" }}>
+                  <Form.Item
+                    {...restField}
+                    name={[name, "ubiProducto"]}
+                    // rules={[{ required: true, message: 'Missing first name' }]}
+                    style={{ margin: 0, width: "auto" }}
+                  >
+                    <Input placeholder="Ubicación" style={{ margin: 0 }} />
+                  </Form.Item>
+                </Col>
+                <Col xs={3} style={{ padding: "0 5px 0 5px" }}>
+                  <Form.Item
+                    {...restField}
+                    name={[name, "cantProducto"]}
+                    // rules={[{ required: true, message: 'Missing first name' }]}
+                    style={{ margin: 0, width: "auto" }}
+                  >
+                    <Input type="number" placeholder="Cantidad" style={{ margin: 0 }} />
+                  </Form.Item>
+                </Col>
+                <Col xs={2} style={{ padding: "0 5px 0 5px" }}>
+                  <Form.Item
+                    {...restField}
+                    name={[name, "unidadProducto"]}
+                    // rules={[{ required: true, message: 'Missing first name' }]}
+                    style={{ margin: 0, width: "auto" }}
+                  >
+                    {/* <Input placeholder="U/M" style={{ margin: 0 }} /> */}
+                    <Select
+                      style={{ width: '100% ', margin: '0' }}
+                      showSearch
+                      placeholder="U/M"
+                      optionFilterProp="children"
+                      // onChange={onChangeHabitación}
+                      onSearch={() => { }}
+                      filterOption={(input, option) =>
+                        option.children.toLowerCase().indexOf(input.toLowerCase()) >= 0
+                      }
+                    >
+                      <Select.Option value={"a"}>a</Select.Option>
+                      <Select.Option value={"a"}>a</Select.Option>
+                    </Select>
+                  </Form.Item>
+                </Col>
+              </Row>
             ))}
-          </Collapse>
           <Form.Item style={{ marginTop: "20px" }}>
             <Button
               type="dashed"
